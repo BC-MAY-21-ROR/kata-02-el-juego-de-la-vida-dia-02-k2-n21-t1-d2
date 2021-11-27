e0ebb7e1706ae44ad352a5ed0d942356f2549c8b
javs1 <59986245+javs1@users.noreply.github.com>
2021-11-26 18:13:43 -0600

    Creating life
    Co-authored-by: alanjosue97 <perezaln.09@gmail.com>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>
    Co-authored-by: rene3255 <ranchoeltecal@gmail.com>

--- index.rb
+++ index.rb
@@ -29,7 +29,26 @@ class Game
 		puts @grid
 	end
 
+	def init_life
+		puts 'Choose location of alive cell: '
+		alive1 = gets
+		edit_grid([alive1.to_i])
+	end
+
+	def edit_grid array
+		array.each do |cel|
+			row = cel/@width
+			pos = (@width * (row + 1) ) - cel
+			pos = @width - pos
+			@grid[row][pos - 1] = '*'
+		end
+		puts ">>>>>>>>>>>>>>>>>"
+		puts @grid
+	end
+
 end
 
 newGame = Game.new(user_height.to_i,user_width.to_i)
 newGame.init_grid
+newGame.init_life
+newGame.search_life
